8f1a9b8038cb71008de8c8936200cf06ccb7e318
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2024-08-07 13:08:03 +0000

    Comments: arg_types

--- scripts/arg_types.py
+++ scripts/arg_types.py
@@ -18,7 +18,14 @@ def timestamp(arg: str) -> int:
     return timestamp
 
 
-def directory(raw_path):
+def directory(raw_path: str) -> str:
+    """
+    Argparse type for an existing directory path.
+
+    :param arg: given argument
+    :return: absolute path to an existing directory
+    :raises argparse.ArgumentTypeError: if the given argument is not an existing directory
+    """
     if not os.path.isdir(raw_path):
         raise argparse.ArgumentTypeError(f'"{raw_path}" is not an existing directory')
     return os.path.abspath(raw_path)
