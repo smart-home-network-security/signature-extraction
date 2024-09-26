4f4d0c2e539a1ec8d9999d82b3a4a0c82ba1a92e
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2024-09-26 06:50:18 +0000

    Do NOT include in signature reference flows that do not occur in other flows

--- signature_extraction/event_signature_extraction.py
+++ signature_extraction/event_signature_extraction.py
@@ -23,7 +23,7 @@ def patterns_to_signature(patterns: List[NetworkPattern]) -> NetworkPattern:
     reference_pattern = patterns_sorted[0]
 
     # Iterate over flows in the reference pattern
-    for i, flow in enumerate(reference_pattern.get_flows()):
+    for i, reference_flow in enumerate(reference_pattern.get_flows()):
 
         # Flow already parsed, skip
         if i in already_parsed_flow_indices:
@@ -31,30 +31,40 @@ def patterns_to_signature(patterns: List[NetworkPattern]) -> NetworkPattern:
 
 
         ## Parse flow
-        result_flow = FlowFingerprint.build_from_flow(flow)
+        potential_flow = FlowFingerprint.build_from_flow(reference_flow)
         already_parsed_flow_indices.add(i)
 
         # Iterate over NetworkPatterns (i.e., lists of FlowFingerprints),
         # to find flows matching the one currently being processed
+        skip = False  # Skip current reference flow if True
         for j, pattern in enumerate(patterns_sorted):
-            # TODO: exception handling
-            #try:
-            index, matched_flow = pattern.find_matching_flow(flow)
-            if j == 0:  # Reference record
-                already_parsed_flow_indices.add(index)
-            result_flow.add_flow(matched_flow)
-            # except ValueError:
-            #     continue
+            try:
+                index, matched_flow = pattern.find_matching_flow(reference_flow)
+            except ValueError:
+                # No matching flow found for the current reference flow
+                # ==> Reference flow is not part of signature
+                # ==> Skip it
+                skip = True
+                break
+            else:
+                # Matching flow found
+                # ==> Add it to the current potential flow
+                if j == 0:  # Reference record
+                    already_parsed_flow_indices.add(index)
+                potential_flow.add_flow(matched_flow)
+
+        if skip:
+            continue
 
         # Remove port if already matched
         for already_matched_port in already_matched_ports:
-            if already_matched_port in result_flow.ports:
-                del result_flow.ports[already_matched_port]
+            if already_matched_port in potential_flow.ports:
+                del potential_flow.ports[already_matched_port]
 
-        fixed_port = result_flow.get_fixed_port()[0]
+        fixed_port = potential_flow.get_fixed_port()[0]
         already_matched_ports.add(fixed_port)
 
-        signature.add_flow(result_flow)
+        signature.add_flow(potential_flow)
 
     return signature
 
